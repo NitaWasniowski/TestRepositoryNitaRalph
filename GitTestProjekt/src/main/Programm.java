ff7766dd419033e459b53850cb414e3c153cdfaa
Ralph Vogt <ralph@docobot.internal>
2021-10-06 09:34:11 +0200

    extract addition to method

--- GitTestProjekt/src/main/Programm.java
+++ GitTestProjekt/src/main/Programm.java
@@ -3,7 +3,11 @@ package main;
 public class Programm {
 	public static void main(String[] args) {
 		int s1 = 2, s2 = 3, s3;
-		s3 = s1 + s2 ;
+		s3 = addNumbers(s1, s2) ;
 		System.out.println("Ergebniss = " + s3);
 	}
+
+  private static int addNumbers(int s1, int s2) {
+    return s1 + s2;
+  }
 }
