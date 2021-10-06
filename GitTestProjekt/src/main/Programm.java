e149cc90096fbcef173d82d6c1308ce47aa4e524
Ralph Vogt <ralph@docobot.internal>
2021-10-06 09:38:47 +0200

    add subtract method

--- GitTestProjekt/src/main/Programm.java
+++ GitTestProjekt/src/main/Programm.java
@@ -2,12 +2,16 @@ package main;
 
 public class Programm {
 	public static void main(String[] args) {
-		int s1 = 2, s2 = 3, s3;
-		s3 = addNumbers(s1, s2) ;
-		System.out.println("Ergebniss = " + s3);
+      int s1 = 2, s2 = 3;
+      System.out.println("Ergebnis Addition = " + addNumbers(s1, s2));
+      System.out.println("Ergebnis Subtraction = " + subtractNumbers(s1, s2));
 	}
 
   private static int addNumbers(int s1, int s2) {
     return s1 + s2;
+  }
+
+  private static int subtractNumbers(int s1, int s2) {
+    return s1 - s2;
   }
 }
